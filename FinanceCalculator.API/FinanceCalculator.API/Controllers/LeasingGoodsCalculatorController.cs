79de2087b89a1b0ad442516166e1b5036b33f016
IliyanSidjimkov <114525107+IliyanSidjimkov@users.noreply.github.com>
2026-01-09 01:25:10 +0200

    Add files via upload

---
+++ FinanceCalculator.API/FinanceCalculator.API/Controllers/LeasingGoodsCalculatorController.cs
@@ -0,0 +1,26 @@
+﻿using FinanceCalculator.API.Contracts;
+using FinanceCalculator.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinanceCalculator.API.Controllers
+{
+
+    [ApiController]
+    [Route("api/leasing-goods")]
+    public class LeasingGoodsCalculatorController :ControllerBase
+    {
+        private readonly ILeasingGoodsCalculatorService _service;
+
+        public LeasingGoodsCalculatorController(ILeasingGoodsCalculatorService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost("calculate")]
+        public ActionResult<LeasingGoodsResponce>Calculate(
+            [FromBody] LeasingGoodsRequest request)
+        {
+            return Ok(_service.Calculate(request));
+        }
+    }
+}
